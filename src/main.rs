eda2e8009b4ec7471f91fe8ae470863048c54b79
ctangam <luochenjoey@gmail.com>
2024-11-04 15:03:55 +0800

    add multiple connection

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 // Uncomment this block to pass the first stage
-use std::{net::TcpListener, io::{Write, BufReader, BufRead, Read}};
 
-fn main() {
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
                 println!("accepted new connection");
                 let s = "+PONG\r\n";
                 loop {
                     let mut buffer = [0; 512];
-                    if let Ok(count) = stream.read(&mut buffer) {
+                    if let Ok(count) = stream.try_read(&mut buffer) {
                         if count == 0 {
                             break;
                         }
                         let req = String::from_utf8_lossy(&buffer[0..count]);
                         println!("{}", req);
-                        stream.write(s.as_bytes()).unwrap();
+                        stream.try_write(s.as_bytes()).unwrap();
                     } else {
                         break;
                     }
