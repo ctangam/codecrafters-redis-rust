7b187ef3efa0971e7085f5c2228fb9d190e53856
ctangam <luochenjoey@gmail.com>
2024-11-11 16:01:00 +0800

    add psync

--- src/cmd/psync.rs
+++ src/cmd/psync.rs
@@ -1,14 +1,14 @@
 use crate::parse::Parse;
 
 pub struct Psync {
-    pub repl_id: u64,
-    pub repl_offset: u64,
+    pub repl_id: String,
+    pub repl_offset: String,
 }
 
 impl Psync {
     pub fn parse_frames(parse: &mut Parse) -> crate::Result<Psync> {
-        let repl_id = parse.next_int()?;
-        let repl_offset = parse.next_int()?;
+        let repl_id = parse.next_string()?;
+        let repl_offset = parse.next_string()?;
 
         Ok(Psync {
             repl_id,
